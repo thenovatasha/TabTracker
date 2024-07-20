dbab75e142745328ef08ce03bfefd2755d76da3f
ntasha <nova.tasha@student.unimelb.edu.au>
2024-07-21 03:04:42 +1000

    Store tabs time in chrome local storage

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,10 @@
-let spentTime = {};
-let prevUrl = null;
-let prevTime = null;
-
+import {
+    startTracking,
+    endTracking,
+    viewCurrentActive,
+    viewUrlCollection,
+} from "./storage.js";
+// chrome.storage.local.clear();
 const regex = /(?<=:\/\/)(?:www\.)?([^\/?#]+)(?:[\/?#]|$)/;
 
 // let matchedUrl = prevUrl.match(regex);
@@ -10,26 +13,54 @@ const regex = /(?<=:\/\/)(?:www\.)?([^\/?#]+)(?:[\/?#]|$)/;
  * Tab Events
  */
 // when the current tab updates
-chrome.tabs.onUpdated.addListener(() => console.log("TABUPDATED!"));
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    console.log("tab: onUpdated\n");
+    console.log(tabId);
+    console.log(changeInfo);
+});
 
-// // when the tab selection changes
-chrome.tabs.onActivated.addListener(() => console.log("ACTIVATED"));
+// when the tab selection changes
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    console.log("tab: onActivated");
+    // get the current tab info and match the url for the domain
+    console.log(activeInfo.tabId);
+    chrome.tabs.get(activeInfo.tabId, async (tab) => {
+        const urlGroups = tab.url.match(regex);
+        // tab might not have a url set yet
+        if (!urlGroups) {
+            return;
+        }
+        // tab has a url set
+        console.log(urlGroups[1]);
+        // end the previous tracking, and start this one
+        await endTracking();
+        await startTracking(urlGroups[1]);
+    });
+});
 
-// // when a tab is closed
-chrome.tabs.onRemoved.addListener(() => {
-    console.log("REMOVED");
+// when a tab is closed
+chrome.tabs.onRemoved.addListener(async () => {
+    console.log("tab: onRemoved");
 });
 
 /**
  * Window events
  */
 
-chrome.windows.onCreated.addListener(() => {
-    console.log("Window created");
-});
+// chrome.windows.onCreated.addListener(() => {
+//     console.log("Window: onCreated");
+// });
 
-// When another window is selected (not created)
+// When another window is selected (even as a result of another
+// window being closed)
 chrome.windows.onFocusChanged.addListener(() => {
-    console.log("WINDOW FOCUS CHANGED");
+    console.log("window: onFocusChanged");
     // query active tab
 });
+
+// In case the window is closed brute force (and not tab by tab)
+chrome.windows.onRemoved.addListener(() => console.log("window: onRemoved"));
+
+chrome.idle.onStateChanged.addListener(() =>
+    console.log("window: onStateChanged")
+);
---
+++ storage.js
@@ -0,0 +1,68 @@
+/*
+ * set the currentActive to the current site url, with the time as current time
+ */
+export async function startTracking(siteUrl) {
+    // set the currentActive to this site
+    let currentTime = Date.now();
+    chrome.storage.local
+        .set({ currentActive: [currentTime, siteUrl] })
+        .then(() => {
+            console.log(`set ${siteUrl}`);
+        });
+
+    // check if the site has a time recorded
+    // if no time is recorded, set it to 0
+    let urlCollection = await chrome.storage.local.get("url");
+    if (Object.keys(urlCollection).length === 0) {
+        urlCollection = { url: {} };
+        urlCollection["url"][siteUrl] = 0;
+        await chrome.storage.local.set({ url: urlCollection["url"] });
+    }
+    if (urlCollection["url"][siteUrl] === undefined) {
+        urlCollection["url"][siteUrl] = 0;
+        await chrome.storage.local.set({ url: urlCollection["url"] });
+    }
+    console.log(await chrome.storage.local.get("url"));
+}
+
+/*
+ * Calculate the time spent, end the tracking by setting the active to null
+ */
+export async function endTracking() {
+    let currentTime = Date.now();
+
+    // calculate the time between this tab's start and end
+    let active = await chrome.storage.local.get("currentActive");
+
+    // if the user starts with an activity that never got recorded
+    if (!active.currentActive) {
+        console.log("current active was null");
+        return;
+    }
+    let activeStarted = active.currentActive[0];
+    let activeUrl = active.currentActive[1];
+    let delta = currentTime - activeStarted;
+
+    // set the time in the url colection and set currentActive to null
+    let urlCollection = await chrome.storage.local.get("url");
+    urlCollection["url"][activeUrl] += delta;
+    console.log("WAS CHECKING URL COLL: ", urlCollection);
+    // update the urlCollection to reflect the change
+    await chrome.storage.local.set({ url: urlCollection["url"] });
+    // disable the currentActive
+    await chrome.storage.local.set({ currentActive: null });
+}
+
+export function viewUrlCollection() {
+    chrome.storage.local.get("url").then((result) => {
+        console.log("URL view\n");
+        console.log(result);
+    });
+}
+
+export function viewCurrentActive() {
+    chrome.storage.local.get("currentActive").then((result) => {
+        console.log("Current Active\n");
+        console.log(result);
+    });
+}
